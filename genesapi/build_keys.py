df0b750c5a0fdd64d436e5a810c6adde5d3a5644
Simon Wörpel <simon.woerpel@medienrevolte.de>
2018-06-23 18:53:32 +0200

    Make key ids graphql conform

--- genesapi/build_keys.py
+++ genesapi/build_keys.py
@@ -20,5 +20,6 @@ def main(args):
     logger.log(logging.INFO, 'Building keys db from `%s` ...' % args.source)
     files = get_files(args.source, lambda x: x.endswith('_de.json'))
     df = pd.DataFrame(json.load(open(f)) for f in files).rename(columns={'code': 'id'})
+    df['id'] = df['id'].str.replace('-', '_')
     df.index = df['id']
     sys.stdout.write(json.dumps(df.T.to_dict()))
